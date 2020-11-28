ae229164a83580134bd3032c39d63136396489d5
gonzalinux <gonzaleonrey@hotmail.com>
2020-11-28 17:22:41 +0100

    arreglos y comentarios

--- group02/MutationOperator.py
+++ group02/MutationOperator.py
@@ -5,10 +5,11 @@ import numpy
 class MutationOperator:
 
     @staticmethod
-    def apply(lista):
+    def apply(lista,F=(random.random() * 2)):
         best = numpy.array(lista[0].vector)
         x1 = numpy.array(lista[1].vector)
         x2 = numpy.array(lista[2].vector)
+        f=F
 
-        #devuelve un vector mutado aplicando de/best/1
-        return best + (random.random()*2) * (x1 - x2)
+        # devuelve un vector mutado aplicando de/best/1
+        return best + f * (x1 - x2)
